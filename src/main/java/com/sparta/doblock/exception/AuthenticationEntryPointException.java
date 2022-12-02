6078305b36a2b61dbe955f0d7d5c3b8760a6a5e0
Ming-gry <yby818@naver.com>
2022-12-02 18:21:45 +0900

    1. Update : User 검색 시 무한스크롤 페이지 10개로 변경, Authentication, Authorization Exception Handler 수정 2. 본문 : Authentication, Authorization Exception 시 Body 로 Error Message 가 가지 않음, HandlerExceptionResolver 사용하여 처리 3. 해결 : FE 요청 사항 및 BE 자체 Refactor

--- src/main/java/com/sparta/doblock/exception/AuthenticationEntryPointException.java
+++ src/main/java/com/sparta/doblock/exception/AuthenticationEntryPointException.java
@@ -1,18 +1,22 @@
 package com.sparta.doblock.exception;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class AuthenticationEntryPointException implements AuthenticationEntryPoint {
 
+    private final HandlerExceptionResolver handlerExceptionResolver;
+
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) {
+        handlerExceptionResolver.resolveException(request, response, null, authException);
     }
 }
--- src/main/java/com/sparta/doblock/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/doblock/exception/GlobalExceptionHandler.java
@@ -1,12 +1,15 @@
 package com.sparta.doblock.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DoBlockExceptions.class)
@@ -22,5 +25,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public ResponseEntity<?> handleMaxUploadSizeExceededException() {
         return ResponseEntity.status(ErrorCodes.EXCEED_FILE_SIZE.getHttpStatus()).body(new ErrorMessage(ErrorCodes.EXCEED_FILE_SIZE.getCode(), ErrorCodes.EXCEED_FILE_SIZE.getMessage()));
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<?> handleAuthenticationException(Exception exception){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> handleAccessDeniedException(Exception exception){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exception.getMessage());
     }
 }
